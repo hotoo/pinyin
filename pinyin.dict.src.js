189477199f44d7c5cb8ac316e4fedc64d4f0b896
闲耘™ <hotoo.cn@gmail.com>
2010-07-30 19:51:30 +0800

    combination multi-yinpin.

--- pinyin.dict.src.js
+++ pinyin.dict.src.js
@@ -494,11 +494,29 @@ var pinyin = function(){
     }
     delete PINYIN_DICT;
 
+    /*
+    * 笛卡尔乘积，返回两个数组的所有可能的组合。
+    * @param {Array}
+    * @param {Array}
+    * @return {Array}
+    */
+    function product(a,b){
+        var r=[];
+        for (var i=0,l=a.length; i<l; i++){
+            for (var j=0,m=b.length; j<m; j++){
+                r[r.length] = (a[i] instanceof Array) ? a[i].concat(b[j]) : [].concat(a[i],b[j]);
+            }
+        }
+        return r;
+    }
+
+
     /**
     * @param {String} hans 要转为拼音的目标字符串（汉字）。
-    * @param {Boolean} single 是否仅返回匹配的第一个拼音。
+    * @param {Boolean} single 是否仅保留匹配的第一个拼音。
     * @param {String} sp 返回结果的分隔符，默认返回数组集合。
-    * @return {String,Array}
+    * @return {String,Array} 如果 sp 为 null，则返回 Array。
+    *                        否则，返回以 sp 分隔的字符串。
     */
     return function(hans, single, sp){
         var len = hans.length;
@@ -514,10 +532,17 @@ var pinyin = function(){
                 if(y){
                     py[py.length] = single?y[0]:y;
                 }else{
-                    py[py.length] = hans.charAt(i);
+                    py[py.length] = single?hans.charAt(i):[hans.charAt(i)];
                 }
             }
-            return single?py.join(sp||""):py;
+            if(single){return sp==null?py:py.join(sp||"");}
+
+            var pys=py[0];
+            var r=pys[0]
+            for (var i=1,l=py.length; i<l; i++){
+                pys = product(pys, py[i]);
+            }
+            return sp==null?pys:pys.join(sp||"");
         }
     }
 }();
