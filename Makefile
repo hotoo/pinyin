ba2da7c2bcba47149e21b7f4e89715fb723585e3
hotoo <hotoo.cn@gmail.com>
2014-02-16 15:41:19 +0800

    Update dict robot.

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@ test:
 publish:
 	@npm publish
 
+ZI_DICT = ./tools/dict/zi-frequent.js
+dict:
+	@echo 'module.exports = {' > $(ZI_DICT)
+	@node ./tools/robot.js >> $(ZI_DICT)
+	@echo '};' >> $(ZI_DICT)
 
 infrequent:
 	@node ./tools/infrequent.js > ./tools/zi/infrequent.js
--- tools/robot.js
+++ tools/robot.js
@@ -1,101 +1,100 @@
- /**
- * @overview
- *
- * @author 闲耘™ (hotoo.cn[AT]gmail.com)
- * @version 2013/01/27
- */
+// 自动抓取单个汉字拼音。
 
 var remote = require("./remote");
 var URLEncoder = require("./urlencode");
-var WORDS = require("./data-words");
 
-var words = "";
-var words_loaded = {};
-for(var i=0,a,l=WORDS.length; i<l; i++){
-  a = WORDS[i][1].split("");
-  for(var j=0,w,m=a.length; j<m; j++){
-    w = a[j];
-    if(words_loaded.hasOwnProperty(w)){continue;}
-    words_loaded[w] = w;
+var request = require("request");
 
-    words += w;
-  }
-}
+var frequent = require("./zi/frequent");
+var frequent2 = require("./zi/frequent2");
 
-// 从快典网查询，带注音的 GBK 编码拼音，Iconv 无法正常转换编码，
-// 而其注音格式参数无效，未完成。
-function getKDDPinyinDict(han){
-  remote("http://py.kdd.cc/index.asp?zy=1&u=3&dy=red&ps=gray&zs=blue&kd=55&wz="+
-    URLEncoder.encode(han), "POST", function(html){
-      var py_start = "<font color=gray>";
-      var py_end = "</font>";
-      var idx = html.indexOf(py_start);
-      var idx2 = html.indexOf(py_end, idx);
-      if(idx < 0){
-        console.log(han, "No Found.")
-        return;
-      }
-      console.log(han, ":", html.substring(idx+py_start.length, idx2));
-      //console.log(html);
-    }, function(response){
-    });
-}
 
 // 各个汉字的读音独立存储。
 var PINYIN_DATA = {};
 // 同音字合并存储。
 var PINYIN_DATA_MIXED = {};
+var SERVER = "http://zi.artx.cn";
 var counts = 0;
 function getPinyinDict(han){
-  remote("http://zi.artx.cn/zi/search/?dsearch_kind=1&dsearch_main="+
-    encodeURIComponent(han)+"&x=0&y=0", "POST", function(html){
-      var py_start = '<span class="style1">拼音：';
-      var py_end = '</span>';
-      var idx = html.indexOf(py_start);
-      var idx2 = html.indexOf(py_end, idx);
-      if(idx < 0){
-        PINYIN_DATA["Unknow"] = han;
-        PINYIN_DATA_MIXED[han] = "Unknow";
-        return;
-      }else{
-        var s_py = html.substring(idx+py_start.length, idx2);
-        var a_py = s_py.split(",");
-        for(var i=0,l=a_py.length; i<l; i++){
-          PINYIN_DATA[han] = s_py;
-          if(!PINYIN_DATA_MIXED.hasOwnProperty(a_py[i])){
-            PINYIN_DATA_MIXED[a_py[i]] = han;
-          }else{
-            PINYIN_DATA_MIXED[a_py[i]] += han;
-          }
+  request(SERVER + "/zi/search/?dsearch_kind=1&dsearch_kind2=on&dsearch_main="+
+      encodeURIComponent(han), "GET", function(error, response, html){
+        if(error || response.statusCode !== 200){
+          console.error("404, Not Found. 0", han);
+          return;
+        }
+
+        var re_zi = /<div class="search_bushou"><div class="title">总笔划 \d+ 画：<\/div><a href="([^"]+)" target="_blank">.+<\/a><\/div>/;
+        var m = html.match(re_zi);
+        if(m && m[1]){
+          var url = m[1];
+
+          request(SERVER + url, function(err, res, body){
+
+            if(error || response.statusCode !== 200){
+              console.error("404, Not Found 1.", han);
+              return;
+            }
+
+            var re_py = /<div><span>拼音：([^<]+)<\/span>/;
+            var m_py = body.match(re_py);
+            if(m_py && m_py[1]){
+
+              var py = m_py[1];
+              console.log('"' + han + '": "' + py + '",');
+
+            }else{
+              console.error("404, Not Found. 2", han);
+            }
+
+          });
+        }else{
+          console.error("404, Not Found. 3", han);
         }
       }
-
-      counts++;
-      // finished.
-      if(words.length === counts){
-        //console.log(JSON.stringify(PINYIN_DATA));
-        console.log(JSON.stringify(PINYIN_DATA_MIXED));
-      }
-    },
-    function(response){
-    }
   );
 }
 
-function chunk(array, process, context){
-    var items = array.concat();   //clone the array
-    setTimeout(function(){
-        var item = items.shift();
-        process.call(context, item);
+// 分时处理数组的每一项数据。
+// @param {Array} array, 待处理的数值数据。
+// @param {Function} process, 处理过程。
+// @param {Object} 处理过程和回调函数中 this 指向的上下文。
+// @param {Function} 分时处理完成的回调函数。
+function chunk(array, process, context, callback){
+  var items = array.concat();   //clone the array
+  var delay = 25;
 
-        if (items.length > 0){
-            setTimeout(arguments.callee, 100);
-        }
-    }, 25);
+  setTimeout(function(){
+    var item = items.shift();
+    process.call(context, item);
+
+    if(items.length > 0){
+      setTimeout(arguments.callee, delay);
+    }else{
+      callback && callback.call(context);
+    }
+  }, delay);
 }
 
-chunk(words.split(""), getPinyinDict);
+chunk("中汉字".split(""), getPinyinDict);
+//chunk((frequent + frequent2).split(""), getPinyinDict);
 
-//for(var i=0,l=words.length; i<l; i++){
-  //getPinyinDict(words.charAt(i));
-//}
+
+// 从快典网查询，带注音的 GBK 编码拼音，Iconv 无法正常转换编码，
+// 而其注音格式参数无效，未完成。
+// @deprecated
+function getKDDPinyinDict(han){
+  remote("http://py.kdd.cc/index.asp?zy=1&u=3&dy=red&ps=gray&zs=blue&kd=55&wz="+
+      URLEncoder.encode(han), "POST", function(html){
+        var py_start = "<font color=gray>";
+        var py_end = "</font>";
+        var idx = html.indexOf(py_start);
+        var idx2 = html.indexOf(py_end, idx);
+        if(idx < 0){
+          console.log(han, "No Found.")
+    return;
+        }
+        console.log(han, ":", html.substring(idx+py_start.length, idx2));
+        //console.log(html);
+      }, function(response){
+      });
+}
