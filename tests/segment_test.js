0c0963b50fb07467d1f91174b5ec7d177ad606bd
aqiongbei <aqiongbei@gmail.com>
2018-08-28 19:09:44 +0800

    Add pinyin-node segment 漯(luo)河

--- tests/segment_test.js
+++ tests/segment_test.js
@@ -7,3 +7,10 @@ console.log(pinyin("重庆大学", {
     heteronym: true,              // 启用多音字模式
     segment: true,                 // 启用分词，以解决多音字问题。
 }));
+
+// test pinyin-node segment 漯(luo)河
+console.log(pinyin("漯河", {segment: true}));
+console.log(pinyin("漯河", {
+    heteronym: true,              // 启用多音字模式
+    segment: true,                 // 启用分词，以解决多音字问题。
+}));
