339da757ff925f29b2c83fe697a526d78d3b417c
闲 耘 <hotoo.cn@gmail.com>
2021-10-20 16:59:54 +0800

    Updates

--- _bundle.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('../lib/web-pinyin.js')
--- benchmark/short.js
+++
@@ -1,9 +0,0 @@
-var memory = process.memoryUsage().rss;
-
-var text = "你好拼音";
-
-var pinyin = require("../web-pinyin");
-console.time("pinyin");
-pinyin(text);
-console.timeEnd("pinyin");
-console.log(process.memoryUsage().rss - memory);
